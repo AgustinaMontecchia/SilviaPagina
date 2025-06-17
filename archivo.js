c26bc1c4ee49b60cb937d0a98543e5cf0fc958b9
Agustina Montecchia <agustinamontecchia98@gmail.com>
2025-06-17 15:23:52 -0300

    agregando slide

---
+++ archivo.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper(".galeria-sobre-mi", {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+    });
+  });
